0b22985d3ad93ec0896a4b706adbc3c92c5b0b32
Dominic Chapman <dominicchapman@me.com>
2017-09-25 22:19:06 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,12 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  # move is to a position on the tic-tac-toe-board
-  # i.e. index in range of 0 to 8
+  # if index.between?(1,9)
+  #   if !position_taken?(board, index)
+  #     true
+  #   end
+  # end
 
-  # move to a vacant position
-  # i.e. position_taken? is false
-  index.between(0,8) && !position_taken?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
