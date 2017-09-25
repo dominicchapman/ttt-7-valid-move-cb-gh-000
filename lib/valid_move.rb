c76d8c8ecff20dbf1c7046a124bb843ed0cf796c
Dominic Chapman <dominicchapman@me.com>
2017-09-25 22:15:30 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,7 @@ def valid_move?(board, index)
 
   # move to a vacant position
   # i.e. position_taken? is false
-  if (index.range(0, 8)) && (position_taken?(board, index) == false)
+  if (index.between(0, 8)) && (position_taken?(board, index) == false)
     return true
   else
     return false
